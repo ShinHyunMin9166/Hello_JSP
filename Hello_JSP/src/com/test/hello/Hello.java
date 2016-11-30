57866d055bcd35a7d380dfd1cb3df36f781a574b
ShinHyunMin <shm6708@naver.com>
2016-11-30 12:33:28 +0900

    package

---
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.test.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("local Repository create Hello_JSP");
+		// 012456:hotfix edit by Shin
+		System.out.println("hotfix edit/modify");
+		// 012456:end
+		
+	}
+
+}
